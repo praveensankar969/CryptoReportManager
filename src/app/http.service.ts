1435f61bcc6da5ea2720a304f6ad88959df24954
Praveen <sankar_praveen@hotmail.com>
2021-10-30 14:36:44 +0530

    Fix: Cors issue FIx:6

--- src/app/http.service.ts
+++ src/app/http.service.ts
@@ -11,16 +11,12 @@ export class HttpService {
   constructor(private http: HttpClient) { }
   
   GetAllTickers(){
-    let headers = new HttpHeaders();
-    headers.append("Access-Control-Allow-Origin", "https://wazirx.com");
-    return this.http.get<Coin[]>("api/v2/tickers", {headers:headers}).pipe(first(), map(res=> {
+    return this.http.get<Coin[]>("api/v2/tickers").pipe(first(), map(res=> {
       return Object.values(res)
     }));
   }
 
   SendMessage(text: string){
-    let headers = new HttpHeaders();
-    headers.append("Access-Control-Allow-Origin", "https://wazirx.com");
-    this.http.get("sendMessage?chat_id=-1001711739464&text="+text+"&parse_mode=html", {headers : headers}).pipe(first()).subscribe();
+    this.http.get("sendMessage?chat_id=-1001711739464&text="+text+"&parse_mode=html").pipe(first()).subscribe();
   }
 }
